587017e8e978207e586c54af3cc6e9f19a242edf
Tarak1009 <anne.tarakanath1009@gmail.com>
2024-12-19 10:09:54 +0530

    'commentsAdded'

--- Basics_of_Python/SocketProgramming/client.py
+++ Basics_of_Python/SocketProgramming/client.py
@@ -1,9 +1,11 @@
 import socket
+
+
 try:
     c = socket.socket()
 
     c.connect(('localhost',9001))
-    print(c.recv(1024).decode())
+    print(c.recv(1024).decode())   # 1024 is the buffer for receiving the data from the server as this is a client
     clientName = input("Enter Your name: ")
     c.send(bytes(clientName, "utf-8"))
 except Exception as e:
--- Basics_of_Python/SocketProgramming/server.py
+++ Basics_of_Python/SocketProgramming/server.py
@@ -2,17 +2,14 @@ import socket
 
 try:
     s = socket.socket()
-
     s.bind(("localhost", 9001))
-
-
-    s.listen(3)
+    s.listen(3)             # listen 3 means it will listen to only 3 connections and if any 4th connection is made then it will reject it.
     print("waiting for connections")
 
     while True:
         c, addr = s.accept()
-        c.send(bytes("Welcome to Server", "utf-8"))
-        name = c.recv(1024).decode()
+        c.send(bytes("Welcome to Server", "utf-8"))     # Remember to send the data in the bytes format with utf-8 encoding
+        name = c.recv(1024).decode()            # 1024 is the buffer for receiving the data from the client as this is a server
         print("Connected successfully ", addr, name)
         c.close()
 
